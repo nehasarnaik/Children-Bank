dc2f175075a0dde2d15f643049d9f56dd266daaf
Om Pathak <ompatthak@gmail.com>
2023-08-30 11:43:14 +0530

    Admin Dashboard Base Setup

---
+++ src/app/pages/admin/admin-withdraw/admin-withdraw.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-withdraw',
+  templateUrl: './admin-withdraw.component.html',
+  styleUrls: ['./admin-withdraw.component.css']
+})
+export class AdminWithdrawComponent {
+
+}
